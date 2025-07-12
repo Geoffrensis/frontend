58fcc96377cf34789732d4d05e8de2b56b08af4c
Geoffrensis <vahegrigoryan179@gmail.com>
2025-07-12 22:10:14 +0400

    Creating deleteBook

--- src/components/BookList/BookList.jsx
+++ src/components/BookList/BookList.jsx
@@ -1,7 +1,13 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import './BookList.css';
+import { deleteBook } from '../../redux/books/actionCreators';
 const BookList = () => {
     const books = useSelector((state) => state.books);
+    const dispatch = useDispatch();
+
+    const handleDeleteBook = (id) => {
+        dispatch(deleteBook(id));
+    };
     return (
         <div className="app-block book-list">
             <div className="filter-row">
@@ -20,6 +26,16 @@ const BookList = () => {
                                     {')'} {book.year}
                                     {book.title} by{' '}
                                     <strong>{book.author}</strong>
+                                </div>
+                                <div className="book-actions">
+                                    <button
+                                        type="button"
+                                        onClick={() =>
+                                            handleDeleteBook(book.id)
+                                        }
+                                    >
+                                        Delete
+                                    </button>
                                 </div>
                             </li>
                         );
